8b3cc83ab6808e96af4d2fc30d0ceda02a79ccdc
Oldscha <olga.shpakova@hotmail.com>
2023-04-24 15:27:14 +0200

    Renamed project, added initial code for shiny app, loaded data.

--- app.r
+++ app.r
@@ -1,59 +1,183 @@
-library(shiny)
+library(shiny) # Package for shiny applications
+library(dplyr) # Package for data manipulations
+library(magrittr) # Package for pipe operator
+library(ggplot2) # Package for creating graphs
+library(shinythemes) # Package for shiny app themes
 
-# Define UI for app that draws a histogram ----
+course_data <- readRDS("data/europe.rds") %>% # Load the course data
+  mutate(AvgTemperatureC = round((AvgTemperatureF - 32)*5/9, 1)) # Create a new column with Avg Temperature in Celsius
+
+# Define UI for application
 ui <- fluidPage(
+  # Specify the selected theme to the 'theme' argument
+  theme = shinytheme("flatly"),
   
-  # App title ----
-  titlePanel("Data Mining Capstone Project 2023"),
+  # Application title
+  titlePanel("COURSE SHINY APP"),
   
-  # Sidebar layout with input and output definitions ----
   sidebarLayout(
-    
-    # Sidebar panel for inputs ----
+    # Sidebar panel
     sidebarPanel(
+      "This is the sidebar panel",
       
-      # Input: Slider for the number of bins ----
-      sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+      # Input: A simple slider ----
+      sliderInput(inputId = "year", label = "Year",
+                  min = 2000,
+                  max = 2019,
+                  step = 1, 
+                  value = 2000,
+                  sep = ''),
+      
+      # Input: A simple drop down list  ----
+      selectInput(inputId = "country", label = "Country:",
+                  choices = c(sort(unique(course_data$Country)))),
+      
+      # Input: A simple drop down list  ----
+      selectInput(inputId = "city", label = "City:",
+                  choices = c(sort(unique(course_data$City)))),
+      
+      # Input: A simple text input  ----
+      textInput(inputId = "text_input", label = "Input text here:"),
+      
+      # Input: A simple radio button input  ----
+      radioButtons(inputId = "temp_scale", label = "Temperature scale:",
+                   choices = list("Fahrenheit" = "fahrenheit",
+                                  "Celsius" = "celsius"),
+                   selected = "celsius"),
+      
+      # Input: Action button that subsets storm data  ----
+      actionButton(inputId = "button", label = "GO!")
       
     ),
     
-    # Main panel for displaying outputs ----
+    # Main panel
     mainPanel(
+      "This is the main panel",
       
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
+      textOutput(outputId = "text_output"),
       
+      # Layout: Tabset with info, data, and plots tabs ----
+      tabsetPanel(type = "tabs",
+                  tabPanel(title = "Info",
+                           h3("App description"),
+                           p("This is the course shiny app.", br(),
+                             "It is created during the course exercises using the europe.rds data:", br(),
+                             strong("Average daily temperatures"), "(in Fahrenheit) from cities around
+                                           Europe from 2000 to 2019"),
+                           hr(),
+                           verbatimTextOutput("data_summary")
+                  ),
+                  tabPanel(title = "Data",
+                           
+                           dataTableOutput("data_table")
+                  ),
+                  tabPanel(title = "Plots",
+                           fluidRow(
+                             column(width = 12, plotOutput("lineplot"))
+                           ),
+                           fluidRow(
+                             column(width = 6, plotOutput("boxplot")),
+                             column(width = 6, plotOutput("lineplotTemp"))
+                           )
+                  )
+      )
     )
   )
 )
 
-# Define server logic required to draw a histogram ----
-server <- function(input, output) {
+# Define server side logic
+server <- function(input, output, session) {
   
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
-  output$distPlot <- renderPlot({
+  country_df <- reactive({
+    course_data %>%
+      filter(Year >= input$year) %>% # Subset the rows to keep data more than or equal to a year
+      filter(Country == input$country) # Subset the rows to keep a specific country
+  })
+  
+  city_df <- eventReactive(input$button, {
+    country_df() %>% 
+      filter(City == input$city) %>% # Subset the rows for specific City
+      filter(Year == input$year) # Subset the rows for specific Year
+  })
+  
+  year_df <- reactive({
+    country_df() %>% 
+      filter(City == input$city) %>% # Subset the rows for specific City
+      filter(Year == input$year) %>%  # Subset the rows for specific Year
+      group_by(Country, City, Year, Month) %>% 
+      summarise(MinTempF = min(AvgTemperatureF),
+                MeanTempF = round(mean(AvgTemperatureF), 1),
+                MaxTempF = max(AvgTemperatureF),
+                MinTempC = min(AvgTemperatureC),
+                MeanTempC = round(mean(AvgTemperatureC), 1),
+                MaxTempC = max(AvgTemperatureC)) %>% 
+      ungroup()
     
-    x    <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  })
+  
+  # Output: Render a text output  ----
+  output$text_output <- renderText({
+    paste("Your inputs are:", input$year, input$country, input$city, input$text_input, input$temp_scale)
+  })
+  
+  # Output: Render a print output  ----
+  output$data_summary <- renderPrint({
+    summary(course_data)
+  })
+  
+  # Output: Render a (dynamic) table output  ----
+  output$data_table <- renderDataTable({
+    city_df()
+  })
+  
+  # Output: Render a plot output  ----
+  output$lineplot <- renderPlot({
+    ggplot(data = city_df()) +
+      geom_line(mapping = aes(x = Date, y = AvgTemperatureF), size = 1) +
+      ylab("Average daily temperatures (in Fahrenheit)")
+  })
+  
+  # Output: Render a plot output  ----
+  output$boxplot <- renderPlot({
+    ggplot(data = country_df()) +
+      geom_boxplot(mapping = aes(x = Month, y = AvgTemperatureF, group = Year))
+  })
+  
+  # Output: Render a plot output  ----
+  output$lineplotTemp <- renderPlot({
     
-    hist(x, breaks = bins, col = "#75AADB", border = "white",
-         xlab = "Waiting time to next eruption (in mins)",
-         main = "Histogram of waiting times")
+    if(input$temp_scale == "fahrenheit"){
+      res <- ggplot(data = year_df()) +
+        geom_line(mapping = aes(x = Month, y = MinTempF), size = 1, colour = "red", linetype = "dotted") +
+        geom_line(mapping = aes(x = Month, y = MeanTempF), size = 1, colour = "black") +
+        geom_line(mapping = aes(x = Month, y = MaxTempF), size = 1, colour = "red", linetype = "dotted") +
+        scale_x_discrete(name = "", limits = month.abb) +
+        ylab("Average daily temperatures (in Fahrenheit)")
+    }
+    
+    if(input$temp_scale == "celsius"){
+      res <- ggplot(data = year_df()) +
+        geom_line(mapping = aes(x = Month, y = MinTempC), size = 1, colour = "red", linetype = "dotted") +
+        geom_line(mapping = aes(x = Month, y = MeanTempC), size = 1, colour = "black") +
+        geom_line(mapping = aes(x = Month, y = MaxTempC), size = 1, colour = "red", linetype = "dotted") +
+        scale_x_discrete(name = "", limits = month.abb) +
+        ylab("Average daily temperatures (in Celsius)")
+    }
+    
+    return(res)
+    
+  })
+  
+  observe({
+    
+    new_choices <- unique(course_data$City[course_data$Country == input$country])
+    
+    updateSelectInput(session, inputId = "city", choices = new_choices)
     
   })
   
 }
 
-# Create Shiny app ----
+# Run the application 
 shinyApp(ui = ui, server = server)
+
